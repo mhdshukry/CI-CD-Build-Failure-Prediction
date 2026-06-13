26d2f335b77f0845fb8176e92b48a748837b7286
Fathima Athiyya <mhdshukry110@gmail.com>
2026-06-14 03:28:23 +0530

    Update test_local_repo.py

--- local_analyzer/test_local_repo.py
+++ local_analyzer/test_local_repo.py
@@ -1,7 +1,12 @@
 from local_repo_analyzer import analyze_latest_commit
 
-repo_path = r"D:\Projects\your-project-folder"
+repo_path = r"D:\CI-CD-Build-Failure-Prediction"
 
 result = analyze_latest_commit(repo_path)
 
-print(result)
+print("\n==============================")
+print("LATEST COMMIT ANALYSIS")
+print("==============================")
+
+for key, value in result.items():
+    print(f"{key}: {value}")
